34d4f58c3db79abb736c22c3eaa4fff457526f2c
Kauan Mascaro <kauanmascaro@gmail.com>
2024-07-25 17:24:23 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,47 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+#define TAM 3
+
+struct tipo_pessoa{
+	int idade;
+	float peso;
+	char nome[50];
+};
+
+typedef struct tipo_pessoa pessoa;
+
+main() {
+	setlocale(LC_ALL,"portuguese");
+	
+	pessoa lista[TAM];
+	int i;
+	
+	for (i=0;i<TAM;i++){
+		printf ("Insira os dados (%d):\n",i+1);
+		puts("Nome: ");
+		scanf ("%50[^\n]s", &lista[i].nome);
+		fflush(stdin);
+		
+		puts("Idade: ");
+		scanf ("%d", &lista[i].idade);
+		fflush(stdin);
+		
+		puts("Peso: ");
+		scanf("%f", &lista[i].peso);
+		fflush(stdin);
+	}
+	
+	system("CLS");
+	
+	puts("Seus dados: \n");
+	for (i=0;i<TAM;i++){
+		printf ("---------- Pessoa %d ----------\n", i+1);
+		printf ("\tNome: %s\n", lista[i].nome);
+		printf ("\tIdade: %d\n", lista[i].idade);
+		printf ("\tPeso: %f\n", lista[i].peso);
+};
+
+}
